062281040467ef1226ef22aa0f8053bd1a494c41
Extreme Electronics <102665314+ExtremeElectronics@users.noreply.github.com>
2024-05-19 17:32:39 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,24 @@
+
+
+//#define USER_AGENT "PicoSip 0.0.1"
+
+//UDP Settings
+//#define LOCAL_PORT 50666 //my port
+#define TRANSPORT_LOWER  "udp"
+#define TRANSPORT_UPPER  "UDP"
+#define SOCKET_RX_TIMEOUT_MSEC 200
+
+//IO 
+#define RING 10
+#define DIAL 11
+#define ANSWER 12
+#define HANGUP 13
+
+#define PARSE_MAX 99
+
+//colours
+#define TXTEXT printf("\e[1;31m"); //red
+#define RXTEXT printf("\e[1;32m"); //green
+#define STATETEXT printf("\e[1;34m"); //blue
+#define CLTEXT printf("\e[m"); //clear
+
